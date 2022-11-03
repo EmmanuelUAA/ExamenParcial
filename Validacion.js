d807f471a21997e80c2b444d8dc20eda67486407
EmmanuelUAA <em2a7n037@gmail.com>
2022-11-03 16:06:02 -0600

    commit inicial

---
+++ Validacion.js
@@ -0,0 +1,34 @@
+/****************OBTIENE LOS DATOS REGISTRADOS EN EL FORMULARIO*********************************************************** */
+const nombre = document.getElementById("name")
+const email = document.getElementById("email")
+const pass = document.getElementById("password")
+const form = document.getElementById("form")
+const parrafo = document.getElementById("warnings")
+
+/**********HACE LA VALIDACION DE LOS DATOS, QUE EL USUARIO TENGA UN TAMAÑO MAYOR A 6, EL CORREO CONTENGA EL @
+ ***********EL .COM ETC********************************************************************* */
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+    parrafo.innerHTML = ""
+    if(nombre.value.length <6){
+        warnings += `El nombre no es valido <br>`
+        entrar = true
+    }
+    if(!regexEmail.test(email.value)){
+        warnings += `El email no es valido <br>`
+        entrar = true
+    }
+    if(pass.value.length < 8){
+        warnings += `La contraseña no es valida <br>`
+        entrar = true
+    }
+
+    if(entrar){
+        parrafo.innerHTML = warnings
+    }else{
+        parrafo.innerHTML = "se ha creado la cuenta"
+    }
+})
